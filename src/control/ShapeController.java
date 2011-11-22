48dea8f70ae38799b5e8568df90ccb41e2e92014
Jan Machala <jan.machala@email.cz>
2011-11-22 13:57:28 +0100

    Modified shape controller skeleton

--- src/control/ShapeController.java
+++ src/control/ShapeController.java
@@ -1,14 +1,27 @@
 package control;
 
+import java.util.Observable;
+import java.util.Observer;
 import model.ShapeModel;
+import view.ShapeView;
 
-public class ShapeController {
+public class ShapeController implements Observer {
 
 	private ShapeModel model;
-	private ShapeController controller;
+	private ShapeView view;
 
-	public ShapeController(ShapeModel model, ShapeController controller) {
-		this.controller = controller;
+	public ShapeController(ShapeModel model, ShapeView view) {
+		initialize(model, view);
+	}
+
+	protected final void initialize(ShapeModel model, ShapeView view) {
+		this.view = view;
 		this.model = model;
+		this.model.addObserver(this);
+	}
+
+	@Override
+	public void update(Observable o, Object o1) {
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 }
