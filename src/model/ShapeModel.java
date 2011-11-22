ec31da9416b85bc0410f4eb2c305c2947f59844e
Jan Machala <jan.machala@email.cz>
2011-11-22 13:55:17 +0100

    Added methods to model for operating with squares

--- src/model/ShapeModel.java
+++ src/model/ShapeModel.java
@@ -1,7 +1,22 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Observable;
+import model.entity.Square;
 
 public class ShapeModel extends Observable {
-	
+
+	private ArrayList<Square> squares = new ArrayList<Square>();
+
+	public int getSquareCount() {
+		return squares.size();
+	}
+
+	public Square getSquare(int index) {
+		return this.squares.get(index);
+	}
+
+	public void addSquare(Square square) {
+		this.squares.add(square);
+	}
 }
